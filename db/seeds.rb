d4cd2cc98dcf51e6b44d8ab9624edeb3b5e684df
Harry Kingdon <harry@harrykingdon.com>
2018-08-16 15:06:16 +0100

    did something STRANGE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.delete_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+hash = JSON.parse(json)
+arr = hash["drinks"]
+arr.each { |pair| Ingredient.create(name: pair["strIngredient1"]) }
